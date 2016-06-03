f81656dad1f52c075b801cbcddc958e85fbab821
luo <353830102@qq.com>
2016-06-03 15:29:28 +0800

    modify

--- source/src/com/nazir/Main.java
+++ source/src/com/nazir/Main.java
@@ -1,10 +1,13 @@
 package com.nazir;
 
+import com.utils.PropertiesUtil;
 import com.utils.UrlUtil;
 
 public class Main {
     public static void main(String[] args) {
-        final ApiTest apiTest = new ApiTest("localhost:8080", null, null, UrlUtil.getInstance().getUrlVersion(), null);
+        final ApiTest apiTest = new ApiTest(
+                PropertiesUtil.getInstance().getConfig("host") + ":" + PropertiesUtil.getInstance().getConfig("port"),
+                null, null, UrlUtil.getInstance().getUrlVersion(), null);
         apiTest.setDefaultCloseOperation(3);
         apiTest.setVisible(true);
     }
